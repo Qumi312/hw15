757a858c20f5892cf72a0fd1aff75c4c3d46dd3c
qumi <bahz1997@gmail.com>
2024-02-29 17:36:59 +0400

    добавлено несколько тестов

--- pytests.py
+++ pytests.py
@@ -12,9 +12,18 @@ def s2():
 def test_init_student(s1):
     assert s1 is not None
 
-def test_init_incorrect():
+def test_incorrect_name():
     with pytest.raises(ValueError):
         Student('евгений 223', 'subjects.csv')
+
+def test_init_incorrect_file():
+    with pytest.raises(FileNotFoundError):
+        Student('Иван', 'subjects.txt')
+
+def test_incorrect_subject_name():
+    with pytest.raises(TypeError):
+        Student.get_average_test_score("Биология")
+
 
 
 
